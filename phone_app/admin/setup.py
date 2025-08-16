836274cefd6d2663a8e42ceac5dfbf1d057fdeec
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-08-16 09:22:53 +0600

    Update setup.py
    fd

--- phone_app/admin/setup.py
+++ phone_app/admin/setup.py
@@ -6,5 +6,5 @@ from phone_app.db.database import engine
 
 def setup_admin(app: FastAPI):
     admin = Admin(app, engine)
-    admin.add_view(UserProfileAdmin)
+    admin.add_view(UserProfileAdmin) 
     admin.add_view(PredictAdmin)      
